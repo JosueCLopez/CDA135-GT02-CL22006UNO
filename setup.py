7a45cf177250298c87591f7f503cd6e0b1a27493
JosueCLopez <cl22006@ues.edu.sv>
2025-04-13 11:38:11 -0600

    Se modifica las carpetas para la referencia de la funcion

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ HERE = pathlib.Path(__file__).parent
 
 setup(
     name='CL22006UNO',
-    version='1.0.0',
+    version='1.0.1',
     author='Josue Israel Colocho Lopez',
     author_email='CL22006@ues.edu.sv',
     description='Liberia para resolver ecuaciones lineales y no lineales sin usar librerias aparte',
@@ -16,6 +16,10 @@ setup(
     url='https://github.com/JosueCLopez/CDA135-GT02-CL22006UNO',
     license='MIT',
     packages=['CL22006UNO'],
-    include_package_data=True,
-    
+    package_dir={'CL22006UNO': 'CL22006UNO'},
+    entry_points={
+        'console_scripts': [
+            'ver = CL22006UNO.ver:main',
+        ],
+    },
 )
